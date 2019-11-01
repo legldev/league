546fd9af914631addc262f32efb8970a2aec0fd2
Luís Garcés <luisgarcesleon@gmail.com>
2019-11-01 17:19:13 -0300

    Updates

---
+++ precache-manifest.f191dc410c4aeebdcbf61cfec17ddf9e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38331547e47ef04726ba1a9f99d688b",
+    "url": "/league/index.html"
+  },
+  {
+    "revision": "9224d565472dbf7cf4ee",
+    "url": "/league/static/css/main.5cce2ab9.chunk.css"
+  },
+  {
+    "revision": "8215ed4d288aacb2d721",
+    "url": "/league/static/js/2.8d92616c.chunk.js"
+  },
+  {
+    "revision": "9224d565472dbf7cf4ee",
+    "url": "/league/static/js/main.5642ee47.chunk.js"
+  },
+  {
+    "revision": "a0ab14f6d5963b8aacee",
+    "url": "/league/static/js/runtime-main.b075c37f.js"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/carolina.b4a99d01.png"
+  },
+  {
+    "revision": "6b577104bb5726919dab8cff25851555",
+    "url": "/league/static/media/ernesto.6b577104.png"
+  },
+  {
+    "revision": "ec070ff55ca2286ff533cfd7cb1689ae",
+    "url": "/league/static/media/felinos.ec070ff5.png"
+  },
+  {
+    "revision": "4fe9d27494f3f26700bf6fd576f24721",
+    "url": "/league/static/media/hugo.4fe9d274.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/ivan.b4a99d01.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/jose.b4a99d01.png"
+  },
+  {
+    "revision": "cc9e20dc36b0cf4c5bfda95a0a8493db",
+    "url": "/league/static/media/juan.cc9e20dc.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/karen.b4a99d01.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/laura.b4a99d01.png"
+  },
+  {
+    "revision": "760178851d8c4d3ff93da020fc619656",
+    "url": "/league/static/media/luis.76017885.png"
+  },
+  {
+    "revision": "567afa70e3eb93737f13a9c0f3f232cd",
+    "url": "/league/static/media/manuel.567afa70.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/mariana.b4a99d01.png"
+  },
+  {
+    "revision": "ceb489d0bae5dac8785c096f175212e8",
+    "url": "/league/static/media/mi-liga-2.ceb489d0.png"
+  },
+  {
+    "revision": "ceb489d0bae5dac8785c096f175212e8",
+    "url": "/league/static/media/mi-liga.ceb489d0.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/league/static/media/paco.392f84eb.png"
+  },
+  {
+    "revision": "e7b4175e4db9c3db98f5ae92e19bf5c8",
+    "url": "/league/static/media/piratas.e7b4175e.png"
+  },
+  {
+    "revision": "eb85c8dc5ab5026ac7119e828f12a4de",
+    "url": "/league/static/media/sergio.eb85c8dc.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/league/static/media/susana.b4a99d01.png"
+  },
+  {
+    "revision": "9752a81329c2504d71ced85663f5e70e",
+    "url": "/league/static/media/tiburones.9752a813.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c4bf74f0950587a0ba4a669be4f7372.js"
+  "/league/precache-manifest.f191dc410c4aeebdcbf61cfec17ddf9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/league/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
