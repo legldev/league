f052d28c9cc9034dc0519c90aa43fd1ceed23d4c
Luís Garcés <luisgarcesleon@gmail.com>
2019-11-01 17:52:10 -0300

    first commit

--- src/Router.js
+++ src/Router.js
@@ -6,16 +6,15 @@ import Calendario from './components/calendario/Calendario';
 import NotFound from './components/common/not-found/NotFound';
 
 const Router = () => (
-   <HashRouter basename='/'>
+    <HashRouter>
         <Switch>
-            <Route exact path='/league' component={Liga} />
-            <Route exact path='https://luisgarcesleon.github.io/league/' component={Liga} />
-            <Route path='https://luisgarcesleon.github.io/league/liga' component={Liga} />
-            <Route path='https://luisgarcesleon.github.io/league/calendario/:equipo' component={Calendario} />
-            <Route path='https://luisgarcesleon.github.io/league/calendarios' component={Calendario} />
+            <Route exact path='/' component={Liga} />
+            <Route path='/liga' component={Liga} />
+            <Route path='/calendario/:equipo' component={Calendario} />
+            <Route path='/calendarios' component={Calendario} />
             <Route component={NotFound} />
         </Switch>
     </HashRouter>
-) 
+)
 
 export default Router;
