678f464004e59dced07a586b38104a57273ad1ec
Luís Garcés <luisgarcesleon@gmail.com>
2019-11-01 17:14:56 -0300

    Updates

---
+++ precache-manifest.0c4bf74f0950587a0ba4a669be4f7372.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7140ef02e156998b695a08a61e4c3982",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19a49c05e4560174b8b6",
+    "url": "/static/css/main.83200fd6.chunk.css"
+  },
+  {
+    "revision": "8215ed4d288aacb2d721",
+    "url": "/static/js/2.8d92616c.chunk.js"
+  },
+  {
+    "revision": "19a49c05e4560174b8b6",
+    "url": "/static/js/main.0a90b6f1.chunk.js"
+  },
+  {
+    "revision": "8bf5272295dad48f9648",
+    "url": "/static/js/runtime-main.30965559.js"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/carolina.b4a99d01.png"
+  },
+  {
+    "revision": "6b577104bb5726919dab8cff25851555",
+    "url": "/static/media/ernesto.6b577104.png"
+  },
+  {
+    "revision": "ec070ff55ca2286ff533cfd7cb1689ae",
+    "url": "/static/media/felinos.ec070ff5.png"
+  },
+  {
+    "revision": "4fe9d27494f3f26700bf6fd576f24721",
+    "url": "/static/media/hugo.4fe9d274.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/ivan.b4a99d01.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/jose.b4a99d01.png"
+  },
+  {
+    "revision": "cc9e20dc36b0cf4c5bfda95a0a8493db",
+    "url": "/static/media/juan.cc9e20dc.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/karen.b4a99d01.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/laura.b4a99d01.png"
+  },
+  {
+    "revision": "760178851d8c4d3ff93da020fc619656",
+    "url": "/static/media/luis.76017885.png"
+  },
+  {
+    "revision": "567afa70e3eb93737f13a9c0f3f232cd",
+    "url": "/static/media/manuel.567afa70.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/mariana.b4a99d01.png"
+  },
+  {
+    "revision": "ceb489d0bae5dac8785c096f175212e8",
+    "url": "/static/media/mi-liga-2.ceb489d0.png"
+  },
+  {
+    "revision": "ceb489d0bae5dac8785c096f175212e8",
+    "url": "/static/media/mi-liga.ceb489d0.png"
+  },
+  {
+    "revision": "392f84eb322df4c270e94b351110f16b",
+    "url": "/static/media/paco.392f84eb.png"
+  },
+  {
+    "revision": "e7b4175e4db9c3db98f5ae92e19bf5c8",
+    "url": "/static/media/piratas.e7b4175e.png"
+  },
+  {
+    "revision": "eb85c8dc5ab5026ac7119e828f12a4de",
+    "url": "/static/media/sergio.eb85c8dc.png"
+  },
+  {
+    "revision": "b4a99d0159a6ec68cd91a4465369a905",
+    "url": "/static/media/susana.b4a99d01.png"
+  },
+  {
+    "revision": "9752a81329c2504d71ced85663f5e70e",
+    "url": "/static/media/tiburones.9752a813.png"
+  }
+]);
