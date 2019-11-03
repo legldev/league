36e598c07d1bbbd90bc2987b12cead48ec616bc8
Luís Garcés <luisgarcesleon@gmail.com>
2019-11-02 21:53:57 -0300

    four

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Liga from './components/liga/Liga';
 import Calendario from './components/calendario/Calendario';
 import NotFound from './components/common/not-found/NotFound';
 
 const Router = () => (
-  <BrowserRouter basename="league">
+    <HashRouter>
         <Switch>
-            <Route exact path='/' component={Liga} />
+            <Route path='/' component={Liga} />
             <Route path='/liga' component={Liga} />
             <Route path='/calendario/:equipo' component={Calendario} />
             <Route path='/calendarios' component={Calendario} />
             <Route component={NotFound} />
-        </Switch>
-    </BrowserRouter>
+    </Switch>    
+    </HashRouter>
 )
 
 export default Router;
